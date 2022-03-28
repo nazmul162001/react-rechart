3bb5072b8a8a9d6cdd1f4e482ee70ddc79a1931b
nazmul162001 <nazmulprime162001@gmail.com>
2022-03-28 11:09:46 +0600

    added 3 line in chart & tolltip

--- src/components/MyLineChart/MyLineChart.js
+++ src/components/MyLineChart/MyLineChart.js
@@ -7,43 +7,43 @@ const MyLineChart = () => {
       name: 'Supplier A',
       price: 4000,
       sales: 2400,
-      amt: 2400,
+      amount: 2400,
     },
     {
       name: 'Supplier B',
       price: 3000,
       sales: 1398,
-      amt: 2210,
+      amount: 2210,
     },
     {
       name: 'Supplier C',
       price: 2000,
       sales: 9800,
-      amt: 2290,
+      amount: 2290,
     },
     {
       name: 'Supplier D',
       price: 2780,
       sales: 3908,
-      amt: 2000,
+      amount: 2000,
     },
     {
       name: 'Supplier E',
       price: 1890,
       sales: 4800,
-      amt: 2181,
+      amount: 2181,
     },
     {
       name: 'Supplier F',
       price: 2390,
       sales: 3800,
-      amt: 2500,
+      amount: 2500,
     },
     {
       name: 'Supplier G',
       price: 3490,
       sales: 4300,
-      amt: 2100,
+      amount: 2100,
     },
   ];
 
@@ -51,6 +51,7 @@ const MyLineChart = () => {
       <LineChart width={800} height={550} data = {data}>
          <Line type="monotone" dataKey="price" stroke="#8884d8" />
          <Line type="monotone" dataKey="sales" stroke="#885411" />
+         <Line type="monotone" dataKey="amount" stroke="#458442" />
          <XAxis dataKey= 'name'></XAxis>
         <YAxis></YAxis>
         <Tooltip></Tooltip>
