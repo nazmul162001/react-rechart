97b1f7e98cdd5c50c7356de061c9f8e00e48ccd5
nazmul162001 <nazmulprime162001@gmail.com>
2022-03-28 15:14:12 +0600

    complete all chart & reacta animation

--- src/App.js
+++ src/App.js
@@ -2,10 +2,23 @@ import logo from './logo.svg';
 import './App.css';
 import MyLineChart from './components/MyLineChart/MyLineChart';
 import SpecialChart from './components/SpecialChart/SpecialChart';
+import { useSpring, animated } from 'react-spring';
+import { useState } from 'react';
 
 function App() {
+  const [flip, set] = useState(false)
+  const props = useSpring({ 
+    to: { opacity: 1 }, 
+    from: { opacity: 0 },
+    reset: true,
+    reverse: flip,
+    delay: 200,
+    onRest: () => set(!flip)
+    })
+
   return (
     <div className="App">
+      return <animated.div style={props}><h1 style={{padding: '15px', fontSize: '50px', color: 'gray'}}>Wellcome To My React-reChart Website</h1></animated.div>
       <MyLineChart />
       <SpecialChart />
     </div>
